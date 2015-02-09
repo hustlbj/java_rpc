398440db0b795d24742821ca554597760a728ae1
hustsselbj <hustlbj@qq.com>
2015-02-09 17:52:37 +0800

    merge thrift java code and thrift_test code

--- thrift_test/src/com/eviac/blog/samples/thrift/server/MyServer.java
+++ thrift_test/src/com/eviac/blog/samples/thrift/server/MyServer.java
@@ -5,11 +5,19 @@ import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TServer.Args;
 import org.apache.thrift.server.TSimpleServer;
+import org.omg.CORBA.PRIVATE_MEMBER;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
+
 import com.eviac.blog.samples.thrift.server.AdditionService;
+import com.eviac.blog.samples.thrift.server.AdditionService.AsyncProcessor;
 
 public class MyServer {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(MyServer.class.getName());
+	
 	public static void StartSimpleServer(AdditionService.Processor<AdditionServiceHandler> processor) {
+		LOGGER.debug("entry");
 		try {
 			TServerTransport serverTransport = new TServerSocket(9090);
 			TServer server = new TSimpleServer(
@@ -19,6 +27,7 @@ public class MyServer {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		LOGGER.debug("leave");
 	}
 
 	public static void main(String[] args) {
