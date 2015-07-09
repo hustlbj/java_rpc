8cae34a291a941d25b67b0a56fb7c83b4a3d9120
hustsselbj <hustlbj@qq.com>
2015-07-09 23:40:45 +0800

    add abstract class and equals()

--- thrift_test/src/test/java/ManagerText.java
+++ thrift_test/src/test/java/ManagerText.java
@@ -19,20 +19,41 @@ public class ManagerText {
 	}
 }
 
-class Employee
+/**
+ * 包含abstract方法的类必须是抽象类
+ * @author BJ
+ *
+ */
+abstract class Person
 {
-	public Employee(String n, double s, int year, int month, int day)
+	public Person(String n)
 	{
 		name = n;
+	}
+	public abstract String getDescription();
+	public String getName()
+	{
+		return name;
+	}
+	private String name;
+}
+
+/**
+ * 必须实现抽象父类的抽象方法
+ * @author BJ
+ *
+ */
+class Employee extends Person
+{
+	//没有隐式的Persion()构造器，必须显式调用 Person的其他构造器
+	public Employee(String n, double s, int year, int month, int day)
+	{
+		super(n);
 		salary = s;
 		GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
 		hireDay = calendar.getTime();
 	}
 	
-	public String getName()
-	{
-		return name;
-	}
 	public double getSalary()
 	{
 		return salary;
@@ -46,11 +67,31 @@ class Employee
 		double raise = salary * byPercent / 100;
 		salary += raise;
 	}
+	public boolean equals(Object otherObject)
+	{
+		//先检查引用是否相等
+		if (this == otherObject) return true;
+		//其次检查otherObject是不是 null
+		if (otherObject == null) return false;
+		//检查类型是否相同
+		if (getClass() != otherObject.getClass())
+			return false;
+		Employee other = (Employee)otherObject;
+		//这里 salary是浮点数。。。。。
+		return getName().equals(other.getName())
+				&& salary == other.salary
+				&& hireDay.equals(other.hireDay);
+	}
 	
 	//子类只能通过getter访问私有域
-	private String name;
 	private double salary;
 	private Date hireDay;
+	
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return String.format("an employee with a salary of $%.2f", salary);
+	}
 }
 
 class Manager extends Employee
@@ -69,6 +110,12 @@ class Manager extends Employee
 	public void setBonus(double b)
 	{
 		bonus = b;
+	}
+	public boolean equals(Object otherObject)
+	{
+		if (!super.equals(otherObject)) return false;
+		Manager other = (Manager)otherObject;
+		return bonus == other.bonus;
 	}
 	private double bonus;
 }
